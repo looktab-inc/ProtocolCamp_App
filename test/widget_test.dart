372d75ac0c8f77c1649b378b377944d7bc52347d
yuchocopie <pointu341@gmail.com>
2023-05-24 18:09:14 +0900

    add google login/

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -8,7 +8,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:tinji/main.dart';
+import 'package:tinji/src/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
